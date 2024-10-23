4aa553fe91fd83972da4f9a371783a38f11f0cbc
PRIME BOT'z <sahidhasannayem@gmail.com>
2024-10-23 19:59:20 +0600

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,19 @@ import os, re
 
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+API_ID = int(os.environ.get("API_ID", "25425840"))
+API_HASH = os.environ.get("API_HASH", "e6ea2eca4aa38e965511f323e5ffa578")
+OWNER = int(os.environ.get("OWNER", "5926160191"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "Prime_Fast_Renamer_Bot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "Prime_Botz")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002184630716"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://prime:prime009911@cluster0.zsu84.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
 STRING = os.environ.get("STRING", "")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
+BOT_PIC = os.environ.get("BOT_PIC", "https://envs.sh/py5.jpg")
 
 
 
